65cec91d649f223a3972dd06884ebf649ea39efd
Elise0n <eliseoa50@gmail.com>
2024-05-28 10:48:22 -0300

    fin?

--- js/script.js
+++ js/script.js
@@ -45,3 +45,10 @@ function mostrarMensaje(mensaje) {
 
 // Agregar el evento submit al formulario
 document.getElementById('contactForm').addEventListener('submit', validarFormulario);
+
+var angle = 0;
+function galleryspin(sign) { 
+spinner = document.querySelector("#spinner");
+if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+}
